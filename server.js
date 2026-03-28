b34b690176630214033122c6b7805a3c3388d83a
mariobismarkbg-stack <mariobismarkbg@gmail.com>
2026-03-27 22:07:33 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const WebSocket = require("ws");
 
 const server = http.createServer((req, res) => {
     // servir index.html
-    let filePath = path.join(__dirname, "index.html");
+   const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, "index.html");
 
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("No se encontró index.html");
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+    });
+});
     fs.readFile(filePath, (err, content) => {
         if (err) {
             res.writeHead(500);
