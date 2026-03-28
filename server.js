e95110ed0f82aef6b0704954492fa89c9e5a77bf
mariobismarkbg-stack <mariobismarkbg@gmail.com>
2026-03-27 21:03:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
-const http = require("http");
-const WebSocket = require("ws");
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Servidor activo");
-});
-
-const wss = new WebSocket.Server({ noServer: true });
-
-server.on("upgrade", (request, socket, head) => {
-    console.log("Upgrade request recibido");
-
-    wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit("connection", ws, request);
-    });
-});
+let players = {};
 
 wss.on("connection", (ws) => {
-    console.log("Jugador conectado");
-
-    ws.send("Conectado correctamente");
+    const id = Math.random().toString(36).substr(2, 9);
+    players[id] = { x: 1000, y: 1000 };
 
     ws.on("message", (msg) => {
-        console.log("Mensaje:", msg.toString());
-        ws.send("OK");
+        try {
+            players[id] = JSON.parse(msg);
+        } catch {}
     });
-});
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto " + PORT);
+    ws.on("close", () => {
+        delete players[id];
+    });
+
+    // enviar estado a todos cada 100ms
+    setInterval(() => {
+        const data = JSON.stringify(players);
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    }, 100);
 });
