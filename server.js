bca776ccef008762956d2861a05548ae59a08c32
mariobismarkbg-stack <mariobismarkbg@gmail.com>
2026-03-27 21:32:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const WebSocket = require("ws");
+
+const server = http.createServer((req, res) => {
+    // servir index.html
+    let filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Error cargando archivo");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content);
+        }
+    });
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+
 let players = {};
+
+server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit("connection", ws, request);
+    });
+});
 
 wss.on("connection", (ws) => {
     const id = Math.random().toString(36).substr(2, 9);
@@ -14,7 +42,6 @@ wss.on("connection", (ws) => {
         delete players[id];
     });
 
-    // enviar estado a todos cada 100ms
     setInterval(() => {
         const data = JSON.stringify(players);
         wss.clients.forEach(client => {
@@ -23,4 +50,9 @@ wss.on("connection", (ws) => {
             }
         });
     }, 100);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
 });
