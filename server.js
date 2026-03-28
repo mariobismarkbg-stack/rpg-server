95eca0ebf3753c88946182abf3e997cdf82907cc
mariobismarkbg-stack <mariobismarkbg@gmail.com>
2026-03-27 19:32:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
+const http = require("http");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({
-    port: process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Servidor activo");
 });
+
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
     console.log("Jugador conectado");
 
     ws.on("message", (msg) => {
         console.log("Mensaje:", msg.toString());
+        ws.send("Recibido: " + msg);
     });
+
+    ws.send("Bienvenido al servidor");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log("Servidor corriendo en puerto " + PORT);
 });
